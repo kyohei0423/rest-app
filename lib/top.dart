449cf0fa0762a91bef3bc6b44d37aa2a1503704d
Kyohei Seo <kyo199333@gmail.com>
2021-08-13 22:53:09 +0900

    easily arrange layout

--- lib/top.dart
+++ lib/top.dart
@@ -3,6 +3,32 @@ import 'package:flutter/material.dart';
 class Top extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.blue);
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('rest'),
+          Text('ストレスとうまくやっていく'),
+          Row(
+            children: [
+              OutlinedButton(
+                  onPressed: () {
+                    print("tap sign up button");
+                  },
+                  child: Text('新規登録する')),
+              OutlinedButton(
+                  onPressed: () {
+                    print("tap login button");
+                  },
+                  child: Text('ログインする'))
+            ],
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+          )
+        ],
+      ),
+    ));
   }
 }
